8fa953bcb3a27c26ea7dc9e187659c18e1ad45f7
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 12:48:00 +0200

    add ServiceFeeding function getAllAnimals

--- backend/src/main/java/de/neuefische/backend/Service/ServiceFeeding.java
+++ backend/src/main/java/de/neuefische/backend/Service/ServiceFeeding.java
@@ -1,9 +1,17 @@
 package de.neuefische.backend.Service;
 
+import de.neuefische.backend.Model.Animals;
+import de.neuefische.backend.Repository.RepoAnimals;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ServiceFeeding {
+    private final RepoAnimals repoAnimals;
+    public List<Animals> getAllAnimals() {
+        return repoAnimals.getAllAnimals();
+    }
 }
