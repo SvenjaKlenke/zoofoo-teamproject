857267c74ed14e23fbc5f5828420977078cea81f
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 17:30:09 +0200

    add requiredArgConstructor in ServiceFeeding and ControllerZooFoo

--- backend/src/main/java/de/neuefische/backend/controller/ControllerZooFoo.java
+++ backend/src/main/java/de/neuefische/backend/controller/ControllerZooFoo.java
@@ -2,6 +2,7 @@ package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.Animals;
 import de.neuefische.backend.service.ServiceFeeding;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api")
+@RequiredArgsConstructor
 
 
 public class ControllerZooFoo {
     private final ServiceFeeding serviceFeeding;
-
-    public ControllerZooFoo(ServiceFeeding serviceFeeding) {
-        this.serviceFeeding = serviceFeeding;
-    }
 
     @GetMapping("/animal")
     public List<Animals> getAllAnimals() {
--- backend/src/main/java/de/neuefische/backend/service/ServiceFeeding.java
+++ backend/src/main/java/de/neuefische/backend/service/ServiceFeeding.java
@@ -2,18 +2,17 @@ package de.neuefische.backend.service;
 
 import de.neuefische.backend.model.Animals;
 import de.neuefische.backend.repository.RepoAnimals;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 
 public class ServiceFeeding {
     private final RepoAnimals repoAnimals;
 
-    public ServiceFeeding(RepoAnimals repoAnimals) {
-        this.repoAnimals = repoAnimals;
-    }
 
     public List<Animals> getAllAnimals() {
         return repoAnimals.getAllAnimals();
