d3276863799fe7d7597a03dcb4ec7acfc121564a
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 12:46:40 +0200

    add Controller function getAllAnimals

--- backend/src/main/java/de/neuefische/backend/Controller/ControllerZooFoo.java
+++ backend/src/main/java/de/neuefische/backend/Controller/ControllerZooFoo.java
@@ -1,14 +1,25 @@
 package de.neuefische.backend.Controller;
 
+import de.neuefische.backend.Model.Animals;
+import de.neuefische.backend.Service.ServiceFeeding;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping("")
+@RequestMapping("api/")
 @RequiredArgsConstructor
 
 public class ControllerZooFoo {
+    private final ServiceFeeding serviceFeeding;
+    @GetMapping("animal")
+    public List<Animals> getAllAnimals() {
+        return serviceFeeding.getAllAnimals();
+    }
+
 
 }
