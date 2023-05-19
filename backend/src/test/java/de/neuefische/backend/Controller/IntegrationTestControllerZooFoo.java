84f7b40a0f901dc6cc4e81e8a6cf5be4636bebec
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 16:51:52 +0200

    update IntegrationTestControllerZooFoo

--- backend/src/test/java/de/neuefische/backend/Controller/IntegrationTestControllerZooFoo.java
+++ backend/src/test/java/de/neuefische/backend/Controller/IntegrationTestControllerZooFoo.java
@@ -1,6 +1,8 @@
 package de.neuefische.backend.Controller;
 
 import de.neuefische.backend.Model.Animals;
+import de.neuefische.backend.Repository.RepoAnimals;
+import de.neuefische.backend.Service.ServiceFeeding;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +11,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,12 +21,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class IntegrationTestControllerZooFoo {
 @Autowired
 MockMvc mockMvc;
-
+RepoAnimals repoAnimals = new RepoAnimals();
+ServiceFeeding serviceFeeding = new ServiceFeeding(repoAnimals);
     @Test
     @DirtiesContext
     void getAllAnimals_returnAllAnimalsAsList_andStatusCode200() throws Exception {
      mockMvc.perform(MockMvcRequestBuilders.get("/api/animal"))
              .andExpect(status().isOk())
              .andExpect(content().json("[]"));
+     verify(serviceFeeding.getAllAnimals());
     }
 }
