b96c85685de56bc69fffda82e39fdfd4809f30f5
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 12:51:11 +0200

    add RepoAnimals function getAllAnimals

--- backend/src/main/java/de/neuefische/backend/Repository/RepoAnimals.java
+++ backend/src/main/java/de/neuefische/backend/Repository/RepoAnimals.java
@@ -1,10 +1,21 @@
 package de.neuefische.backend.Repository;
 
+import de.neuefische.backend.Model.Animals;
 import lombok.Data;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @Data
 
 public class RepoAnimals {
+    private final Map<String, Animals> allAnimalsMap = new HashMap<>();
+
+    public List<Animals> getAllAnimals() {
+        return new ArrayList<>(allAnimalsMap.values());
+    }
 }
