5d7a9d74b4cec72881d6eb5342c274f0b6ba2913
Kristina <kristina.marienfeld@gmail.com>
2023-05-19 11:25:01 +0200

    add animalCard

--- frontend/src/AnimalCard/AnimalCard.tsx
+++ frontend/src/AnimalCard/AnimalCard.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import "./AnimalCard.css";
+import exp from "constants";
 
+type Props = {
+    animals: Animal
+}
 
-function AnimalCard(props) {
+export type Animal = {
+    id:string;
+    image:string;
+    animalSpecies:string;
+    numberOfAnimals:number;
+    keeper:string;
+    feedingStatus:string;
+    food:string;
+    foodAmount:number;
+}
+
+function AnimalCard(props:Props) {
 
     return (
-        <div>
-
+        <div className="AnimalCard">
+            <img src={props.animals.image} alt={"anmial picture"}></img>
+            <h2>{props.animals.animalSpecies}</h2>
+            <p>{props.animals.numberOfAnimals}</p>
         </div>
     );
 }
