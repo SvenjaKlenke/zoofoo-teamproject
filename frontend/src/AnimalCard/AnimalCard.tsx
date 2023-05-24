361d1f822feedd28366b2989f16e384a766f56b4
Kristina <kristina.marienfeld@gmail.com>
2023-05-24 16:52:32 +0200

    add detailsButton and function onClickHandlerForDetails in AnimalCard.tsx

--- frontend/src/AnimalCard/AnimalCard.tsx
+++ frontend/src/AnimalCard/AnimalCard.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import "./AnimalCard.css";
 import {Animal} from "../model/AnimalModel";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     animals: Animal
 }
 
 function AnimalCard(props:Props) {
+    const navigate = useNavigate();
+
+    function onClickHandlerForDetails() {
+        navigate("/animal" + props.animals.id)
+    }
 
     return (
         <div className="AnimalCard">
             <img src={props.animals.pictureOfAnimal} alt={"animal"}></img>
             <h2>{props.animals.species}</h2>
             <p>Amount: {props.animals.numberOfAnimals}</p>
+            <button onClick={onClickHandlerForDetails}>DETAILS</button>
         </div>
     );
 }
