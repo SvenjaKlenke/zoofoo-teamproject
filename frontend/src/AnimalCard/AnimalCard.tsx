d36663dd8e5807221c3c4128827ec31725bf3671
Kristina <kristina.marienfeld@gmail.com>
2023-05-24 17:03:59 +0200

    add css to detailsButton

--- frontend/src/AnimalCard/AnimalCard.tsx
+++ frontend/src/AnimalCard/AnimalCard.tsx
@@ -18,8 +18,11 @@ function AnimalCard(props:Props) {
         <div className="AnimalCard">
             <img src={props.animals.pictureOfAnimal} alt={"animal"}></img>
             <h2>{props.animals.species}</h2>
-            <p>Amount: {props.animals.numberOfAnimals}</p>
-            <button onClick={onClickHandlerForDetails}>DETAILS</button>
+            <p>Amount: {props.animals.numberOfAnimals}
+                <br/>
+                <br/>
+                <button className="detailsButton" onClick={onClickHandlerForDetails}>DETAILS</button>
+            </p>
         </div>
     );
 }
