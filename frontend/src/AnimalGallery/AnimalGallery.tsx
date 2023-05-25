2a74ba2a91734dd6421ee9c3a649dbcc84f68458
amelie <amelieka@gmx.de>
2023-05-25 14:28:25 +0200

    1. add div's to AnimalGallery.tsx to filter by feedStatus 2. Add basic AnimalGallery.css

--- frontend/src/AnimalGallery/AnimalGallery.tsx
+++ frontend/src/AnimalGallery/AnimalGallery.tsx
@@ -3,17 +3,39 @@ import "./AnimalGallery.css";
 import {Animal} from "../model/AnimalModel";
 import AnimalCard from "../AnimalCard/AnimalCard";
 
-
 type Props = {
     animals: Animal[];
 }
 
 function AnimalGallery(props: Props) {
+
+    const feedingNone = props.animals.filter(currentAnimal => currentAnimal.feedStatus === "NONE")
+    const feedingOpen = props.animals.filter(currentAnimal => currentAnimal.feedStatus === "OPEN")
+    const feedingDoing = props.animals.filter(currentAnimal => currentAnimal.feedStatus === "DOING")
+    const feedingDone = props.animals.filter(currentAnimal => currentAnimal.feedStatus === "DONE")
+
     return (
         <div className="Gallery-AnimalCard">
-            {props.animals.map((currentAnimal) => <AnimalCard key={currentAnimal.id} animals={currentAnimal}/>)}
+
+            <div className="card">
+                <h2>AllAnimals</h2>
+                {feedingNone.map(animal => <AnimalCard animals={animal}/>)}
+            </div>
+            <div className="card">
+                <h2>ToFeed</h2>
+                {feedingOpen.map(animal => <AnimalCard animals={animal}/>)}
+            </div>
+            <div className="card">
+                <h2>Feeding</h2>
+                {feedingDoing.map(animal => <AnimalCard animals={animal}/>)}
+            </div>
+            <div className="card">
+                <h2>Fed</h2>
+                {feedingDone.map(animal => <AnimalCard animals={animal}/>)}
+            </div>
         </div>
     );
 }
 
 export default AnimalGallery;
+
