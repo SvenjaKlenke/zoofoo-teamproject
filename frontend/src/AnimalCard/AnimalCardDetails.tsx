1bb5976bd0ed0b3c36359df1eb37a0b550874ce6
Kristina <kristina.marienfeld@gmail.com>
2023-05-25 09:57:53 +0200

    add css for detailPage

--- frontend/src/AnimalCard/AnimalCardDetails.tsx
+++ frontend/src/AnimalCard/AnimalCardDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Animal} from "../model/AnimalModel";
 import {Link, useParams} from "react-router-dom";
+import "./AnimalCardDetails.css"
 
 type Props = {
     animals: Animal []
@@ -15,18 +16,20 @@ function AnimalCardDetails(props: Props) {
 
 
     return (
-        <div>
-            <h1>Details of: {actualAnimal?.species}</h1>
-            <img src={actualAnimal?.pictureOfAnimal} alt="pictureOfAnimal"></img>
-            <br/>
-            <p>Amount: {actualAnimal?.numberOfAnimals}</p>
-            <br/>
-            <p>Food: {actualAnimal?.food}</p>
-            <br/>
-            <p>Food amount: {actualAnimal?.foodAmount}</p>
-            <br/>
-            <br/>
-            <Link to={"/"}>back</Link>
+        <div className="detailsContainer">
+            <div className="detailsCard">
+                <h1>{actualAnimal?.species}</h1>
+                <img src={actualAnimal?.pictureOfAnimal} alt="pictureOfAnimal"></img>
+                <br/>
+                <p>Amount: {actualAnimal?.numberOfAnimals}</p>
+                <br/>
+                <p>Food: {actualAnimal?.food}</p>
+                <br/>
+                <p>Food amount: {actualAnimal?.foodAmount}</p>
+                <br/>
+                <br/>
+                <Link to={"/"}>back</Link>
+            </div>
         </div>
     );
 }
