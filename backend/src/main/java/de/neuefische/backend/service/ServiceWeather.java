10a305be66eee29093792a6985150b503dc26e1d
Kristina <kristina.marienfeld@gmail.com>
2023-05-28 00:05:37 +0200

    add annotation

--- backend/src/main/java/de/neuefische/backend/service/ServiceWeather.java
+++ backend/src/main/java/de/neuefische/backend/service/ServiceWeather.java
@@ -12,13 +12,13 @@ import java.util.Objects;
 @Service
 public class ServiceWeather {
 
-    WebClient webClient = WebClient.create("https://api.openweathermap.org/");
+    WebClient webClient = WebClient.create("https://api.openweathermap.org/data/2.5/weather?lat=21.3069&lon=157.8583&appid=78cf5dedfcd4a63d188233d2779482b3");
 
     public Weather getTemperature() {
 
         WeatherResponse response =
                 Objects.requireNonNull(webClient.get())
-                        .uri("data/2.5/weather?lat=21.3069&lon=157.8583&appid=78cf5dedfcd4a63d188233d2779482b3")
+                        .uri("")
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .retrieve()
                         .toEntity(WeatherResponse.class)
