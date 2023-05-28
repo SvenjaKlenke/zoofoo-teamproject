5bd6bb7a28f2924beba8ca5740d8228309780081
Kristina <kristina.marienfeld@gmail.com>
2023-05-29 00:06:31 +0200

    add test exceptionTest for ServiceWeather function getTemperature

--- backend/src/test/java/de/neuefische/backend/service/ServiceWeatherTest.java
+++ backend/src/test/java/de/neuefische/backend/service/ServiceWeatherTest.java
@@ -1,8 +1,11 @@
 package de.neuefische.backend.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ServiceWeatherTest {
 
@@ -28,4 +31,18 @@ class ServiceWeatherTest {
         //Then
 
     }*/
+
+    @Test
+    void getTemperatureThrowsException() {
+        //Mocking
+        WebClient webClientMock = mock(WebClient.class);
+        ServiceWeather serviceWeather = new ServiceWeather();
+
+        when(webClientMock.get()).thenReturn(null);
+
+        try {
+            serviceWeather.getTemperature();
+        } catch (NullPointerException e) {
+        }
+    }
 }
