8df3f4cda5deb399cb38bb9a9df31901cccf9c02
amelie <amelieka@gmx.de>
2023-05-30 17:36:01 +0200

    add KeeperControllerTest.java

---
+++ backend/src/test/java/de/neuefische/backend/controller/KeeperControllerTest.java
@@ -0,0 +1,36 @@
+package de.neuefische.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class KeeperControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "user", password = "123")
+    void getUserControllerOnly() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "123")
+    void getUserFromEverywhere() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+}
