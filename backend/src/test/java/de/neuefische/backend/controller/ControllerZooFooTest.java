efebf6b1fbfef8af86600a4203bd0963cedbe13a
amelie <amelieka@gmx.de>
2023-06-01 12:15:45 +0200

    update tests

--- backend/src/test/java/de/neuefische/backend/controller/ControllerZooFooTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/ControllerZooFooTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,6 +29,7 @@ class ControllerZooFooTest {
     ObjectMapper objectMapper;
 
     @Test
+    @DirtiesContext
     @WithMockUser(username = "user", password = "1234")
     void getAllAnimals_returnAllAnimalsAsList_andStatusCode200() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/animal"))
@@ -37,6 +39,7 @@ class ControllerZooFooTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser(username = "user", password = "1234")
     void postNewAnimal_expectSuccessfulPost() throws Exception {
         String actual = mockMvc.perform(
                         post("/api/animal")
@@ -53,7 +56,7 @@ class ControllerZooFooTest {
                                                         "pictureOfAnimal":""
                                         }
                                         """)
-                )
+                                .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                                         {
@@ -77,30 +80,32 @@ class ControllerZooFooTest {
     }
     @DirtiesContext
     @Test
+    @WithMockUser(username = "user", password = "1234")
     void whenChangeAnimalStatus_ThenReturnUpdatedAnimalItem_AndStatusCode200() throws Exception {
         mockMvc.perform(post("/api/animal")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                        {
-                                                        "id": "2",
-                                                        "species":"Elephant",
-                                                        "food":"Gras",
-                                                        "foodAmount":10
-                        }
-                        """)
-                )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                                                "id": "2",
+                                                                "species":"Elephant",
+                                                                "food":"Gras",
+                                                                "foodAmount":10
+                                }
+                                """)
+                        .with(csrf()))
                 .andExpect(status().isOk());
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/animal/"+"2")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("""
-                        {
-                                                        "id": "2",
-                                                        "species":"Elephant",
-                                                        "food":"Weed",
-                                                        "foodAmount":20
-                        }
-                        """))
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/animal/" + "2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                                                "id": "2",
+                                                                "species":"Elephant",
+                                                                "food":"Weed",
+                                                                "foodAmount":20
+                                }
+                                """)
+                        .with(csrf()))
                     .andExpect(status().isOk())
                     .andExpect(content().json("""
                         {
@@ -117,30 +122,32 @@ class ControllerZooFooTest {
 
     @DirtiesContext
     @Test
+    @WithMockUser(username = "user", password = "1234")
     void whenChangeAnimalStatus_ThenTrowException_AndStatusCode400() throws Exception {
         mockMvc.perform(post("/api/animal")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
-                        {
-                                                        "id": "2",
-                                                        "species":"Elephant",
-                                                        "food":"Gras",
-                                                        "foodAmount":10
-                        }
-                        """)
-                )
+                                {
+                                                                "id": "2",
+                                                                "species":"Elephant",
+                                                                "food":"Gras",
+                                                                "foodAmount":10
+                                }
+                                """)
+                        .with(csrf()))
                 .andExpect(status().isOk());
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/animal/"+"5")
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/animal/" + "5")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
-                        {
-                                                        "id": "2",
-                                                        "species":"Elephant",
-                                                        "food":"Weed",
-                                                        "foodAmount":20
-                        }
-                        """))
+                                {
+                                                                "id": "2",
+                                                                "species":"Elephant",
+                                                                "food":"Weed",
+                                                                "foodAmount":20
+                                }
+                                """)
+                        .with(csrf()))
                 .andExpect(status().is(400))
                 .andExpect(status().reason(containsString("The id in the url does not match the request body's id")));
     }
