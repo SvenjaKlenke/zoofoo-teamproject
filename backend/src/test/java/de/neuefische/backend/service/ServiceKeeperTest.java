545a231a1df1c91faa57c316c5fd03d60862295c
svenjaklenke <svenja.klenke@gmx.de>
2023-06-02 18:25:59 +0200

    add test getAllKeepers Service

---
+++ backend/src/test/java/de/neuefische/backend/service/ServiceKeeperTest.java
@@ -0,0 +1,36 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.model.Animal;
+import de.neuefische.backend.model.FeedingState;
+import de.neuefische.backend.model.Keeper;
+import de.neuefische.backend.repository.RepoAnimals;
+import de.neuefische.backend.repository.RepoKeeper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ServiceKeeperTest {
+
+    RepoKeeper repoKeeper = mock(RepoKeeper.class);
+    Keeper keeper1 = new Keeper(
+            "1",
+            "Amelie",
+            "12345"
+            );
+
+    ServiceKeeper serviceKeeper = new ServiceKeeper(repoKeeper);
+    @Test
+    void getAllKeepers_shouldReturnAListOfAllKeepers() {
+        //GIVEN
+        when(repoKeeper.findAll()).thenReturn(List.of(keeper1));
+        //WHEN
+        List<Keeper> actual = serviceKeeper.getAllKeepers();
+
+        //THEN
+        verify(repoKeeper).findAll();
+        assertEquals(actual, List.of(keeper1));
+    }
+}
